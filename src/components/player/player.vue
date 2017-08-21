56bded700ca8347d17e3527ecc777260689134cf
Marroar <mar_person@163.com>
2017-08-21 21:59:31 +0800

    歌曲的播放功能

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -20,7 +20,7 @@
                 <div class="middle">
                     <div class="middle-l">
                         <div class="cd-wrapper" ref="cdWrapper">
-                            <div class="cd">
+                            <div class="cd" :class="cdCls">
                                 <img :src="currentSong.image" alt="" class="image">
                             </div>
                         </div>
@@ -35,7 +35,7 @@
                             <i class="icon-prev"></i>
                         </div>
                         <div class="icon i-center">
-                            <i class="icon-play"></i>
+                            <i @click="togglePlaying" :class="playIcon"></i>
                         </div>
                         <div class="icon i-right">
                             <i class="icon-next"></i>
@@ -50,19 +50,21 @@
         <transition name="mini">
             <div class="mini-player" v-show="!fullScreen" @click="open">
                 <div class="icon">
-                    <img src="" alt="" width="40" height="40" :src="currentSong.image">
+                    <img :class="cdCls" width="40" height="40" :src="currentSong.image">
                 </div>
                 <div class="text">
                     <h2 class="name" v-html="currentSong.name"></h2>
                     <p class="desc" v-html="currentSong.singer"></p>
                 </div>
                 <div class="control">
+                    <i @click.stop="togglePlaying" :class="miniIcon"></i>
                 </div>
                 <div class="control" >
                     <i class="icon-playlist"></i>
                 </div>
             </div>
         </transition>
+        <audio ref="audio" :src="currentSong.url"></audio>
     </div>
 </template>
 <script>
@@ -74,10 +76,20 @@
     const transitionDuration = prefixStyle('transitionDuration')
     export default {
         computed: {
+            cdCls() {
+                return this.playing ? 'play' : 'play pause'
+            },
+            playIcon() {
+                return this.playing ? 'icon-pause' : 'icon-play'
+            },
+            miniIcon() {
+                return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+            },
             ...mapGetters([
                 'fullScreen',
                 'playList',
-                'currentSong'
+                'currentSong',
+                'playing'
             ])
         },
         methods: {
@@ -113,7 +125,6 @@
                 })
 
                 animations.runAnimation(this.$refs.cdWrapper, 'move', done)
-
             },
             afterEnter() {
                 animations.unregisterAnimation('move')
@@ -128,6 +139,9 @@
             afterLeave() {
                 this.$refs.cdWrapper.style.transition = ''
                 this.$refs.cdWrapper.style[transform] = ''
+            },
+            togglePlaying() {
+                this.setPlayingState(!this.playing)
             },
             _getPosAndScale() {
                 const targetWidth = 40
@@ -145,8 +159,22 @@
                 }
             },
             ...mapMutations({
-                setFullScreen: 'SET_FULL_SCREEN'
+                setFullScreen: 'SET_FULL_SCREEN',
+                setPlayingState: 'SET_PLAYING_STATE'
             })
+        },
+        watch: {
+            currentSong() {
+                this.$nextTick(() => {
+                    this.$refs.audio.play()
+                })
+            },
+            playing(newPlaying) {
+                const audio = this.$refs.audio
+                this.$nextTick(() => {
+                    newPlaying ? audio.play() : audio.pause()
+                })
+            }
         }
     }
 </script>
